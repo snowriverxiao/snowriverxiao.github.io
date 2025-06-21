484f5fb42d178c4aefc918640e67e968328c4e38
snowriverxiao <57077238+snowriverxiao@users.noreply.github.com>
2025-06-21 18:57:36 -0500

    Update simulation_code.py

--- simulation_code.py
+++ simulation_code.py
@@ -6,3 +6,22 @@ def newvec(n=3):
     random_index = random.randint(0, n - 1)  
     my_list[random_index] = 1
     return np.array(my_list)
+    
+exchange = 0
+Awin = 0
+Bwin = 0
+n = 3
+times = 100000
+for s in range(times):
+    vec=newvec(n)
+    i=random.randint(0, n - 1)
+    A=vec[i]
+    B=vec[-i]
+    mask = np.ones(3, dtype=bool) #
+    mask[i] = False
+    B = vec[mask]
+    j=random.randint(0, n - 2)
+    if B[j] == 0:
+        exchange+=1
+        Awin+=A
+        Bwin+=B[n-2-j]
