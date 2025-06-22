7f5e87fbed73290b0c5b995027b81668b29b0388
snowriverxiao <57077238+snowriverxiao@users.noreply.github.com>
2025-06-21 19:12:47 -0500

    Update simulation_code.py

--- simulation_code.py
+++ simulation_code.py
@@ -8,29 +8,29 @@ def newvec(n=3):
     my_list[random_index] = 1
     return np.array(my_list)
     
-switching = 0
-Awin = 0
-Bwin = 0
-n = 3  ## Try increasing number of doors
-times = 100000
 
-for s in range(times):
-    vec = newvec(n)
-    i = random.randint(0, n - 1)
-    A = vec[i]
-    B = vec[-i]
-    mask = np.ones(n, dtype=bool) 
-    mask[i] = False
-    B = vec[mask]
-    shuffled_indices = np.random.permutation(n-1)
-    random_indices = shuffled_indices[:n-2]
-    if np.sum(B[random_indices]) == 0:
-        switching += 1
-        Awin += A
-        Bwin += B[shuffled_indices[n-2]]
-        
-print(f"The probability of winning with the initial choice {Awin/switching}")
-print(f"The probability of winning with the switching {Bwin/switching}")
+def main(n, times): ## times is the simulation times, n is the number of doors
+    switching = 0
+    Awin = 0
+    Bwin = 0
+
+    for _ in range(times):
+        vec = newvec(n)
+        i = random.randint(0, n - 1)
+        A = vec[i]
+        B = vec[-i]
+        mask = np.ones(n, dtype=bool) 
+        mask[i] = False
+        B = vec[mask]
+        shuffled_indices = np.random.permutation(n-1)
+        random_indices = shuffled_indices[:n-2]
+        if np.sum(B[random_indices]) == 0:
+            switching += 1
+            Awin += A
+            Bwin += B[shuffled_indices[n-2]]
+            
+    print(f"The probability of winning with the initial choice {Awin/switching}")
+    print(f"The probability of winning with the switching {Bwin/switching}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Monty Hall Simulation")
